fb8275f673bec08541117b42f8d6a9aff3a782e5
KURNIAWAN SATRIA <satganzdevs@gmail.com>
2026-09-26 06:15:30 +0700

    Update link-preview.ts

--- src/Utils/link-preview.ts
+++ src/Utils/link-preview.ts
@@ -50,6 +50,7 @@ export const getUrlInfo = async (
 
 		const info = await getLinkPreview(previewLink, {
 			...opts.fetchOpts,
+			resolveDNSHost: true,
 			followRedirects: 'follow',
 			handleRedirects: (baseURL: string, forwardedURL: string) => {
 				const urlObj = new URL(baseURL)
